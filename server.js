87ec9430783d60d7f74cd7b04b88fa08ec12e8ea
WilliamSpruyt <williamspruyt@hotmail.com>
2018-04-21 20:38:11 +0100

    cors fiddle 4

--- server.js
+++ server.js
@@ -29,12 +29,21 @@ app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 app.use(cors());
-app.use(function(req, res, next) {
+
+
+app.get('/products/:id', function(req, res, next) {
+  res.json({
+    msg: 'This is CORS-enabled for all origins!'
+  });
+});
+
+
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
-});
+});*/
 const port = process.env.PORT || 8080;
 app.use('/stats', statRoutes);
 const server = app.listen(port, function() {
