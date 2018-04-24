d5706d66ade837299060b8b38ccb532a3944a01b
WilliamSpruyt <williamspruyt@hotmail.com>
2018-04-24 01:03:13 +0100

    get with http!

--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -34,6 +34,10 @@ export class StatsService {
     console.log('in the service ' + stats.id)
     return this.restangular.all('stats/add').post(stats);
   }
-
+  private handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+  }
 
 }
